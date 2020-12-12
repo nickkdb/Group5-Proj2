8d0ef85d1473b78141e6f38121e9289883376371
kshaq777 <kshaq777@gmail.com>
2020-12-12 10:25:04 -0500

    initial file structure for MY TEAM

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// TODO - Routes
+// require("./routes/html-routes.js")(app);
+// require("./routes/author-api-routes.js")(app);
+// require("./routes/post-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
