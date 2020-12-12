2b1c7e933b27b656c9791299280be7fdd9690e7d
Sam Scibetta <sammieeccentric@gmail.com>
2020-12-12 11:14:00 -0500

    skeleton of public files

---
+++ public/js/header.js
@@ -0,0 +1,8 @@
+document.write('<center><p> THIS IS WHERE THE HEADER HTML WILL BE PLACED INCLUDING THE NAV BAR</p></center>')
+
+
+
+//instructions for adding this to the html
+//<BODY>
+//<script language="javascript" type="text/javascript" 
+  //src="js/header.js"></script>
