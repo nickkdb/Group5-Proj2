dc1c6319d75f472a88ef35533e5b117fe68374ce
kaitlinkenney <kaitlin.kenney96@gmail.com>
2020-12-18 18:00:53 -0500

    added function for deleting playlists in playlist.js

--- public/js/playlist.js
+++ public/js/playlist.js
@@ -1,24 +1,24 @@
-$(document).ready (function(){
+$(document).ready(function () {
 
     // grab all songs to use as needed for validation
     var allSongs;
     $.ajax("/api/songs", {
         type: "GET"
-    }).then (function(results) {
+    }).then(function (results) {
         allSongs = results;
         console.log(allSongs)
     });
 
     // grab the playlist id from the url
     var url = window.location.href;
-    var id = url[url.length-1];
+    var id = url[url.length - 1];
     console.log(id);
-    
+
     // grab all of the playlist songs to display on page
     var plSongs;
     $.ajax(`/api/playlists/${id}`, {
         type: "GET"
-    }).then (function(results) {
+    }).then(function (results) {
         console.log(results)
 
         // set the headers equal to playlist data
@@ -29,77 +29,77 @@ $(document).ready (function(){
         var songs = results.playlistSong;
 
         // create a row for each song in the playlist
-        for (let i=0; i < songs.length; i++){
+        for (let i = 0; i < songs.length; i++) {
             let songRow = `<li>${songs[i].title} | ${songs[i].artist} | ${songs[i].album} </li>`
             $('#plSongs').append(songRow);
         }
     });
 
     // add a song and add to playlist; validate if a song already exists or not
-    $(document).on("click",".addSong", function(event) {
+    $(document).on("click", ".addSong", function (event) {
 
         event.preventDefault()
         //   console.log('TEST CREATE USER');
-       
+
         console.log($(this).data('title'));
-        
-            // the new song to add
-            var newSong = {
+
+        // the new song to add
+        var newSong = {
             title: $(this).data('title').trim(),
             artist: $(this).data('artist').trim(),
             album: $(this).data('album').trim(),
             // genre: $('#genre').val().trim(),
             playlistId: id,
-            };
+        };
 
-            // song - the id to query its existence in DB
-            var querySong = {    
-                title: $(this).data('title').trim(),
-                artist: $(this).data('artist').trim(),
-                album: $(this).data('album').trim()
+        // song - the id to query its existence in DB
+        var querySong = {
+            title: $(this).data('title').trim(),
+            artist: $(this).data('artist').trim(),
+            album: $(this).data('album').trim()
+        }
+        console.log(querySong);
+
+        var match;
+        var oldId;
+
+        // see if the current song matches in the DB
+        for (let i = 0; i < allSongs.length; i++) {
+            if (querySong.title === allSongs[i].title && querySong.artist === allSongs[i].artist && querySong.album === allSongs[i].album) {
+                match = true
+                oldId = allSongs[i].id
             }
-            console.log(querySong);
-
-            var match;
-            var oldId;
-
-            // see if the current song matches in the DB
-            for (let i=0; i < allSongs.length; i++) {
-                if (querySong.title === allSongs[i].title && querySong.artist === allSongs[i].artist && querySong.album === allSongs[i].album ) {
-                    match = true
-                    oldId = allSongs[i].id
-                }
-                else {
-                    match = false;
-                }
-            }
-
-            // if it matches, don't create new song; just create new association in playlist_songs
-            if (match === true) {
-                $.ajax("/api/ps", {
-                    type: "POST",
-                    data: {playlistId: newSong.playlistId, songId: oldId}
-                })
-            }
-            // otherwise, create new song and new association
             else {
-                $.ajax("/api/songs", {
-                    type: "POST",
-                    data: newSong
-                    }).then(
-                    function(results) {
-                        console.log("created new song");
-                        // need to decide where to redirect users
-                     }
-                    );
+                match = false;
             }
-            location.reload();
-        
-        });
+        }
+
+        // if it matches, don't create new song; just create new association in playlist_songs
+        if (match === true) {
+            $.ajax("/api/ps", {
+                type: "POST",
+                data: { playlistId: newSong.playlistId, songId: oldId }
+            })
+        }
+        // otherwise, create new song and new association
+        else {
+            $.ajax("/api/songs", {
+                type: "POST",
+                data: newSong
+            }).then(
+                function (results) {
+                    console.log("created new song");
+                    // need to decide where to redirect users
+                }
+            );
+        }
+        location.reload();
+
+    });
 
 
     // Grab spotify tokens and make a call 
-    $("#searchSpotify").on("click", function(event){
+    $("#searchSpotify").on("click", function (event) {
         event.preventDefault();
 
         // Get the values from the search form
@@ -108,12 +108,12 @@ $(document).ready (function(){
         search = search.replace(/ /g, '%20');
         console.log(search, type);
 
-       // make an ajax call to get the spotify tokens
-       // then make spotify call for data
-       // then append html with data
+        // make an ajax call to get the spotify tokens
+        // then make spotify call for data
+        // then append html with data
         $.ajax("/api/tokens", {
             type: "GET"
-        }).then(function(key){
+        }).then(function (key) {
             $.ajax(`https://api.spotify.com/v1/search?q=${search}&limit=10&type=${type}`, {
                 type: "GET",
                 headers: {
@@ -121,12 +121,12 @@ $(document).ready (function(){
                     'Content-Type': 'application/json',
                     'Authorization': `${key.tokenType} ${key.accessToken}`
                 }
-            }).then(function(results){
+            }).then(function (results) {
                 console.log(results)
                 var spot;
                 var spotDiv = $('#spotResults');
 
-                if (type === 'artist'){
+                if (type === 'artist') {
                     spot = results.artists.items;
                     spotDiv.empty();
 
@@ -135,12 +135,12 @@ $(document).ready (function(){
                     spotDiv.append(resultTable);
 
                     // append rows
-                    for (let i=0; i < spot.length; i++) {
+                    for (let i = 0; i < spot.length; i++) {
                         let row = `<tr>
                             <td>${spot[i].name}</td>
                             <td>${spot[i].followers.total}</td>
                             <td><button class="viewAlb">View Albums</button></td>
-                            <td><button class="viewSongs">View Albums</button></td>
+                            <td><button class="viewSongs">View Songs</button></td>
                         </tr>`;
 
                         $('#table').append(row);
@@ -156,7 +156,7 @@ $(document).ready (function(){
                     spotDiv.append(resultTable);
 
                     // table rows
-                    for (let i=0; i < spot.length; i++) {
+                    for (let i = 0; i < spot.length; i++) {
                         let row = `<tr>
                             <td name="title">${spot[i].name}</td>
                             <td name="album">${spot[i].album.name}<img src="${spot[i].album.images[2].url}"></td>
@@ -178,7 +178,7 @@ $(document).ready (function(){
                     spotDiv.append(resultTable);
 
                     // table rows
-                    for (let i=0; i < spot.length; i++) {
+                    for (let i = 0; i < spot.length; i++) {
                         let row = `<tr>
                             <td>${spot[i].name}</td>
                             <td>${spot[i].artists[0].name}</td>
@@ -191,13 +191,35 @@ $(document).ready (function(){
                     }
                 }
 
-                
-        
+
+
             });
-            
+
         })
 
     })
+
+    // Function for handling what happens when the delete button is pressed
+    $("#deletePlaylist").on("click", function () {
+
+        //targets playlist that was clicked
+        let deleted = $(this).data(id);
+
+        console.log(deleted);
+
+        //ajax call to delete a playlist
+        $.ajax("/api/playlists" + deleted, {
+            type: "DELETE"
+        }).then(
+            function () {
+                console.log("deleted id ", deleted);
+
+                location.reload();
+            }
+        );
+    });
+
+
 
     // $(document).on("click",".addQ", function(event) {
     //     var qdiv = $('#q');
@@ -214,10 +236,10 @@ $(document).ready (function(){
     //     var qdiv = $('#q');
 
     //     var trackId = $(this).attr('id');
-         
+
     //     var embed = 
     //     `<iframe src="https://open.spotify.com/embed/track/${trackId}" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`
-    
+
 
     //     qdiv.append(embed);
 
