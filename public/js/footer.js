23a6d123c18d6f1b01d43a241b8e74cd8ba0e07a
Sam Scibetta <sammieeccentric@gmail.com>
2020-12-20 16:08:15 -0500

    finished styling the create playlist page

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,4 +1,4 @@
-document.write('<br><br><footer><div class="uk-align-left"><img src="images/5wire_logo.png" width="200px" height="auto"></div><ul class="footer-nav"><li href="index.html">Home</li><li href="account.html">Account</li><li href="create-playlist.html">Create Playlist</li><li href="#">Logout</li></ul><p>Design & Development by Group 5<br> &#xA9; Copyright 2020 All Rights Reserved</p></footer>')
+document.write('<br><br><footer><div class="uk-align-left"><img src="images/5wire_logo.png" width="200px" height="auto"></div><ul class="footer-nav"><li href="index.html">Home</li><li href="account.html">Account</li><li href="create-playlist.html">Create Playlist</li></ul><p>Design & Development by Group 5<br> &#xA9; Copyright 2020 All Rights Reserved</p></footer>')
 
 
 
