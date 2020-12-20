45b32a73092321439d3271c1a11d633e75e681fe
Sam Scibetta <sammieeccentric@gmail.com>
2020-12-20 18:28:30 -0500

    added a mobile nav that changes with the screen size

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,4 +1,4 @@
-document.write('<br><br><footer><div class="uk-align-left"><img class="footer-img"src="images/5wire_logo.png" width="100px" height="auto"></div><p>Design & Development by Group 5<br> &#xA9; Copyright 2020 All Rights Reserved</p></footer>')
+document.write('<br><br><br><br><br><br><footer><div class="uk-align-left"><img class="footer-img"src="images/5wire_logo.png" width="100px" height="auto"></div><p>Design & Development by Group 5<br> &#xA9; Copyright 2020 All Rights Reserved</p></footer>')
 
 
 
