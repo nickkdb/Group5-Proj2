cf5d9dae243180bdcb554434fc5cd07486a14f55
Sam Scibetta <sammieeccentric@gmail.com>
2020-12-20 15:44:54 -0500

    added iframe to home page and corrected the login/join butto, and removed logout from the nav

--- public/js/header.js
+++ public/js/header.js
@@ -8,7 +8,7 @@
 
 
 
-document.write('<center>  <nav class="uk-navbar-container uk-margin" uk-navbar><div class="uk-navbar-center"><div class="uk-navbar-center-left"><div><ul class="uk-navbar-nav"><li><a href="/">Home</a></li><li><a href="/account">Account</a></li><li><a href="/create">Create Playlist</a></li></ul></div></div><a class="uk-navbar-item uk-logo" href="index.html"><img src="images/5wire_logo.png" width="100px"height="auto"></a><div class="uk-navbar-center-right"><div><ul class="uk-navbar-nav"><li><a href="#">Log Out</a></li></ul></div></div></div></nav></center>')
+document.write('<nav class="uk-navbar-container uk-margin" uk-navbar><div class="uk-navbar-center"><div class="uk-navbar-center-left"><div><ul class="uk-navbar-nav"><li><a href="/">Home</a></li><li><a href="/account">Account</a></li></ul></div></div><a class="uk-navbar-item uk-logo" href="index.html"><img src="images/5wire_logo.png" width="100px"height="auto"></a><div class="uk-navbar-center-right"><div><ul class="uk-navbar-nav"><li><a href="/create">Create Playlist</a></li></ul></div></div></div></nav>')
 
 
 //instructions for adding this to the html
